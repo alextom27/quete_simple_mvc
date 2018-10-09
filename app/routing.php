8c362e407cd8d8258c4fe5985ec06d5b9cad071b
Alexandre Tomaszewski <42070542+alextom27@users.noreply.github.com>
2018-10-09 11:23:07 +0200

    MVC3

---
+++ app/routing.php
@@ -0,0 +1,12 @@
+<?php
+$routes = [
+    'Item' => [ // Controller
+        ['index', '/', ['GET','POST']], // action, url, HTTP method
+        ['show', '/item/{id}', ['GET','POST']], // action, url, HTTP method
+    ],
+    'Category' => [
+        ['index', '/categories', ['GET', 'POST']],
+        ['show', '/category/{id}', ['GET','POST']],
+
+    ]
+];
--- public/index.php
+++ public/index.php
@@ -1,7 +1,18 @@
 <?php
-require '../vendor/autoload.php';
-use Controller\ItemController;
-$ItemController = new Controller\ItemController();
-$ItemController->index();
+use Controller\CategoryController;
 
-?>
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../app/dispatcher.php';
+
+
+
+    $route = $_GET['route'] ?? '';
+
+ /*   if ($route === 'items') {
+        $controller = new Controller\ItemController;
+        $controller->index();
+    } else {
+        header("HTTP/1.0 404 Not Found");
+        echo 'Page introuvable';
+    }
+ */
