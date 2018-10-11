dbeb1d23d01dd1f5fcf44a967fb7ca62ba3bb69a
Alexandre Tomaszewski <alextom27@gmail.com>
2018-10-11 08:47:28 +0200

    correction

--- public/index.php
+++ public/index.php
@@ -1,12 +1,8 @@
 <?php
 use Controller\CategoryController;
-
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../app/dispatcher.php';
-
-
-
-    $route = $_GET['route'] ?? '';
+ $route = $_GET['route'] ?? '';
 
  /*   if ($route === 'items') {
         $controller = new Controller\ItemController;
